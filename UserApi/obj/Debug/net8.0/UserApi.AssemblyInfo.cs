1bd935f0cd65d0ef2dbee07180374cfa0c80cfa6
TienPN0403 <ngomanhduongsn2003@gmail.com>
2025-10-11 23:30:14 +0700

    fix push

--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145e08f508e6d7ccd0cca681560a4738da4ef3c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60963032f318986604d28dc917781136287dad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
